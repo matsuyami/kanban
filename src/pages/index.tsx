0c247ff7147a17e1930870d570f300bd9055b4ea
matsuyami <mkadwaki@gmail.com>
2022-12-21 17:17:13 -0800

    fix: changed export

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import Layout from '../components/layout/Layout'
+import { Layout } from '../components/layout/Layout'
 import { Board } from '../components/kanban/Board'
 
 const IndexPage = () => (
