4fb6a6c2375438a3dfeb2d8060fe7661ed57061d
matsuyami <mkadwaki@gmail.com>
2023-01-11 21:16:08 -0800

    fix: added apos

--- src/components/modal/ColumnModal.tsx
+++ src/components/modal/ColumnModal.tsx
@@ -1,5 +1,5 @@
 import { useRef, useContext } from 'react'
-import { useForm, useFieldArray, Controller } from 'react-hook-form'
+import { useForm, useFieldArray } from 'react-hook-form'
 import { useOutsideClick } from '../../hooks/useOutsideClick'
 import { v4 as uuidv4 } from 'uuid';
 
@@ -7,7 +7,6 @@ import { BoardContextType } from '../../interfaces/Board'
 import { BoardContext } from '../../context/boardContext'
 import { IBoard } from '../../interfaces/Board'
 import { Task } from '../../interfaces/Task'
-import { InputList } from './InputList';
 
 export const ColumnModal = ({ showModal, setShowModal }) => {
   const MAX_NUMBERS_OF_INPUTS = 6
@@ -69,7 +68,7 @@ export const ColumnModal = ({ showModal, setShowModal }) => {
               {...registerInput(index)}
               className={'relative border-2 rounded border-light-lines dark:bg-dark-gray dark:border-dark-lines w-full pl-4 mb-4 h-[2.5rem]'}
             />
-            {isError(index) && <div className={'absolute text-red top-[0.5rem] right-0 px-4'}><span>Can't be empty</span></div>}
+            {isError(index) && <div className={'absolute text-red top-[0.5rem] right-0 px-4'}><span>Can&apos;t be empty</span></div>}
           </div>
         ))
       }
